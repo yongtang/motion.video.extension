a01438a18f3ab728a47d1da9eb45be339bc440f2
Yong Tang <yong.tang.github@outlook.com>
2025-03-07 18:50:31 +0000

    Add basic loop

--- motion/extension.py
+++ motion/extension.py
@@ -17,17 +17,41 @@ class MotionExtension(omni.ext.IExt):
             server = config.get("nats_ws_url", "ws://localhost:8080") or server
         except Exception as e:
             print("[MotionExtension] Extension config: {}".format(e))
-
         print("[MotionExtension] Extension server: {}".format(server))
+
+        self.server = server
 
         self.ws_server = websocket.WebSocketServer()
 
     def on_startup(self, ext_id):
+        async def f():
+            while self.running:
+                print("[MotionExtension] Extension websocket")
+                await asyncio.sleep(2)
+
+        self.running = True
         loop = asyncio.get_event_loop()
+        self.server_task = loop.create_task(f())
+
         loop.create_task(self.ws_server.start())
         print("[MotionExtension] Extension startup")
 
     def on_shutdown(self):
+        async def f():
+            if (
+                getattr(self, "server_task")
+                and self.server_task
+                and not self.server_task.done()
+            ):
+                self.server_task.cancel()
+                try:
+                    await self.server_task
+                except Exception as e:
+                    print("[MotionExtension] Extension cancel")
+
+        self.running = False
         loop = asyncio.get_event_loop()
+        loop.run_until_complete(f())
+
         loop.run_until_complete(self.ws_server.stop())
         print("[MotionExtension] Extension shutdown")
