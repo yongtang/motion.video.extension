e2e1437537bd42f5749665a7ff78962f1b43228d
Yong Tang <yong.tang.github@outlook.com>
2025-03-12 01:14:42 +0000

    Add policy

--- motion/extension.py
+++ motion/extension.py
@@ -3,17 +3,24 @@ import omni.usd
 import omni.kit.app
 from omni.isaac.sensor import Camera
 from omni.isaac.core.articulations import Articulation
-import asyncio, websockets, toml, json, os, socket
+from omni.isaac.dynamic_control import _dynamic_control
+from scipy.spatial.transform import Rotation as R
+import asyncio, websockets, toml, json, os, socket, io
 import numpy as np
+import PIL.Image
 
 
 class MotionExtension(omni.ext.IExt):
     def __init__(self):
         super().__init__()
 
-        camera = None
-        articulation = None
-        server = "ws://localhost:8081"
+        self.config = {
+            "camera": None,
+            "effector": None,
+            "articulation": None,
+            "server": "ws://localhost:8081",
+        }
+
         try:
             ext_manager = omni.kit.app.get_app().get_extension_manager()
             ext_id = ext_manager.get_extension_id_by_module(__name__)
@@ -22,26 +29,60 @@ class MotionExtension(omni.ext.IExt):
             print("[MotionExtension] Extension config: {}".format(config))
             config = toml.load(config)
             print("[MotionExtension] Extension config: {}".format(config))
-            camera = config.get("camera", camera) or camera
-            articulation = config.get("articulation", articulation) or articulation
-            server = config.get("server", server) or server
+            self.config["camera"] = (
+                config.get("camera", self.config["camera"]) or self.config["camera"]
+            )
+            self.config["effector"] = (
+                config.get("effector", self.config["effector"])
+                or self.config["effector"]
+            )
+            self.config["articulation"] = (
+                config.get("articulation", self.config["articulation"])
+                or self.config["articulation"]
+            )
+            self.config["server"] = (
+                config.get("server", self.config["server"]) or self.config["server"]
+            )
         except Exception as e:
             print("[MotionExtension] Extension config: {}".format(e))
-        print("[MotionExtension] Extension camera: {}".format(camera))
-        print("[MotionExtension] Extension articulation: {}".format(articulation))
-        print("[MotionExtension] Extension server: {}".format(server))
+        print("[MotionExtension] Extension config: {}".format(self.config))
 
-        self.camera = camera
-        self.articulation = articulation
-        self.server = server
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def on_startup(self, ext_id):
         async def f(self):
+            context = omni.usd.get_context()
+            while context.get_stage() is None:
+                print("[MotionExtension] Extension world wait")
+                await asyncio.sleep(0.5)
+            print("[MotionExtension] Extension world ready")
+
+            stage = context.get_stage()
+            print("[MotionExtension] Extension stage {}".format(stage))
+
+            if (
+                self.config["camera"]
+                and stage.GetPrimAtPath(self.config["camera"]).IsValid()
+            ):
+                self.camera = Camera(prim_path=self.config["camera"])
+                self.camera.initialize()
+                print("[MotionExtension] Extension camera {}".format(self.camera))
+            else:
+                self.camera = None
+
+            if self.config["articulation"]:
+                self.articulation = Articulation(self.config["articulation"])
+                print(
+                    "[MotionExtension] Extension articulation {} ({})".format(
+                        self.articulation, self.articulation.dof_names
+                    )
+                )
+
+        async def g(self):
             try:
                 while self.running:
                     try:
-                        async with websockets.connect(self.server) as ws:
+                        async with websockets.connect(self.config["server"]) as ws:
                             await ws.send("SUB test.subject 1\r\n")
                             while self.running:
                                 try:
@@ -70,6 +111,22 @@ class MotionExtension(omni.ext.IExt):
                                                 data
                                             )
                                         )
+                                        self.position = np.array(
+                                            (
+                                                data["position"]["x"],
+                                                data["position"]["y"],
+                                                data["position"]["z"],
+                                                data["orientation"]["x"],
+                                                data["orientation"]["y"],
+                                                data["orientation"]["z"],
+                                                data["orientation"]["w"],
+                                            )
+                                        )
+                                        print(
+                                            "[MotionExtension] Extension position: {}".format(
+                                                self.position
+                                            )
+                                        )
 
                                 except asyncio.TimeoutError:
                                     pass
@@ -83,31 +140,6 @@ class MotionExtension(omni.ext.IExt):
             finally:
                 print("[MotionExtension] Extension server exit")
 
-        async def g(self):
-            context = omni.usd.get_context()
-            while context.get_stage() is None:
-                print("[MotionExtension] Extension world wait")
-                await asyncio.sleep(0.5)
-            print("[MotionExtension] Extension world ready")
-
-            stage = context.get_stage()
-            print("[MotionExtension] Extension stage {}".format(stage))
-
-            if self.camera and stage.GetPrimAtPath(self.camera).IsValid():
-                self.camera = Camera(prim_path=self.camera)
-                self.camera.initialize()
-                print("[MotionExtension] Extension camera {}".format(self.camera))
-            else:
-                self.camera = None
-
-            if self.articulation:
-                self.articulation = Articulation(self.articulation)
-                print(
-                    "[MotionExtension] Extension articulation {} ({})".format(
-                        self.articulation, self.articulation.dof_names
-                    )
-                )
-
         async def v(self):
             try:
                 while self.running:
@@ -116,9 +148,13 @@ class MotionExtension(omni.ext.IExt):
                         if len(image):
                             image = np.array(image, dtype=np.uint8)  # RGBA
                             assert image.shape[-1] == 4, "camera {}".format(image.shape)
-                            image = image[:, :, :3][:, :, ::-1]  # BGR
+                            image = image[:, :, :3]  # Remove alpha channel, keep RGB
                             assert image.shape[-1] == 3, "camera {}".format(image.shape)
-                            self.socket.sendto(image.tobytes(), ("127.0.0.1", 6000))
+                            buffer = io.BytesIO()
+                            PIL.Image.fromarray(image, mode="RGB").save(
+                                buffer, format="JPEG2000", quality_mode="lossless"
+                            )
+                            self.socket.sendto(buffer.getvalue(), ("127.0.0.1", 6000))
                     except asyncio.CancelledError:
                         raise
                     except Exception as e:
@@ -129,15 +165,41 @@ class MotionExtension(omni.ext.IExt):
             finally:
                 print("[MotionExtension] Extension camera exit")
 
+        self.position = None
+        self.step_position = None
+
         self.running = True
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(g(self))
-        self.server_task = loop.create_task(f(self))
+        loop.run_until_complete(f(self))
+        self.server_task = loop.create_task(g(self))
         self.camera_task = loop.create_task(v(self)) if self.camera else None
         print("[MotionExtension] Extension startup")
 
+        # self.subscription = (
+        #    omni.kit.app.get_app()
+        #    .get_update_event_stream()
+        #    .create_subscription_to_pop(self.step, name="StepFunction")
+        # )
+
+    def step(self, delta: float):
+        print("[MotionExtension] Extension step {}".format(delta))
+
+        if self.position is not None:
+            value = self.position
+            if self.step_position is not None:
+                delta_p = value[:3] - value[:3]
+                delta_r = (
+                    R.from_quat(value[3:]) * R.from_quat(value[3:]).inv()
+                ).as_quat()
+                print(
+                    "[MotionExtension] Extension step {} {} {}".format(
+                        delta_p, delta_r, delta
+                    )
+                )
+            self.step_position = value
+
     def on_shutdown(self):
-        async def f(self):
+        async def g(self):
             if getattr(self, "server_task") and self.server_task:
                 self.server_task.cancel()
                 try:
@@ -159,6 +221,6 @@ class MotionExtension(omni.ext.IExt):
 
         self.running = False
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(f(self))
+        loop.run_until_complete(g(self))
         loop.run_until_complete(v(self))
         print("[MotionExtension] Extension shutdown")
